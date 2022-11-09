563379c09d17ef83ead35eb5e9c117c90ca35a41
crisnta <torresalvarezcristobal@gmail.com>
2022-11-09 18:52:45 -0300

    test first fetch post

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,27 @@
-import Head from 'next/head'
+import type { NextPage } from 'next'
+import axios from 'axios'
 
-export default function Home() {
+// interface IProps {
+//   videos: 
+// }
+
+const Home = ({ videos } ) => {
+  console.log(videos)
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
   )
+}
+export default Home;
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`)
+
+  return {
+    props: {
+      videos: data 
+    }
+  }
+
 }
