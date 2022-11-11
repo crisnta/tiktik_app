3684327bba0d24cbe3847130f7c584cc8ee8cc6b
crisnta <torresalvarezcristobal@gmail.com>
2022-11-11 19:17:04 -0300

    refac

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -55,7 +55,7 @@ const Comments = ({ comment, setComment, addComment, comments, isPostingComment 
                         </div>
                       </Link>
                       <div>
-                        <p className='-mt-5 ml-16 text-[16px] mr-8'>
+                        <p className='-mt-5 ml-16 text-[16px] mr-8' key={idx}>
                           {item.comment}
                         </p>
                       </div>
